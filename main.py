47c08d30590dbed6061e7e2b1243b1e7883f16ee
nsarmin <nsarmin@miners.utep.edu>
2020-09-02 21:25:26 -0500

    change main.py file

--- main.py
+++ main.py
@@ -1,19 +1,37 @@
 import pickle
 import re
-import os
-file = open("declaration.txt", "r")
-data = file.read()
-#keeps track of the total the number of times each word occurs in the text file
-#print len(words)
-for line in file:
-    line=file.strip()  #remove the leading spaces and newline characters
-    line= line.lower()
-    piece=line.split(" ") #split into words
-    for word in piece:
-        if word in file:
-            piece[word]=piece[word]+1
-        else:
-            piece[word]=1
+import sys
 
-for key in list(piece.keys()):
-    print(key, ":", piece[key])
+def get_word_list(text):
+   # Receives a string containing a document
+   text = text.lower()  #convert all uppercase strings to lowercase
+   emptyList = []      # Created empty list
+   words = ''        #
+   for i in text:
+      if ord(i)>=97 and ord(i)<=122:
+         words = words + i
+      else:
+         if len(words)>0:
+            emptyList.append(words)
+            words = ''
+   return emptyList
+def main():
+   inputFile = sys.argv[1]
+   outputFile = sys.argv[2]
+   files = open(inputFile, 'r', encoding="utf8")
+   text = files.read()
+   files.close()
+   text = get_word_list(text)
+   text = sorted(text)
+   dict = {}
+   for word in text:
+      if word not in dict:
+         dict[word] = 1
+      else:
+         dict[word] = dict[word]+ 1
+   files = open(outputFile, "w")
+   for word in dict:
+      files.write(word+" "+str(dict.get(word))+"\n")
+   files.close()
+   if __name__== "__main__":
+      main()
